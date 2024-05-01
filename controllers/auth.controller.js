db8b94ce20010d04faf1f267c7367a2e145bbcc6
Naziser <SeregaSwapp@mail.ru>
2024-05-02 01:05:19 +0300

    feat: full authorization api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,13 @@
 const db = require("../db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { secret } = require("../config.js");
+
+const generateAccessToken = (id, roles) => {
+  const payload = { id, roles };
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
 
 class AuthController {
   async registration(req, res) {
@@ -10,9 +18,12 @@ class AuthController {
         [username]
       );
       if (candidate.rows.length > 0) {
-        return res
-          .status(400)
-          .json({ message: "Пользователь с таким именем уже существует" });
+        return res.status(400).json({
+          response: {
+            status: 400,
+          },
+          message: "Пользователь с таким именем уже существует",
+        });
       }
       const hashPassword = bcrypt.hashSync(password, 7);
       // Получение ID роли "USER"
@@ -22,7 +33,7 @@ class AuthController {
       );
       // Создание нового пользователя
       const user = await db.query(
-        "INSERT INTO users (username, password, full_name, phone, email) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        "INSERT INTO users (username, password, full_name, phone, email) VALUES ($1, $2, $3, $4, $5) RETURNING id, username, full_name, phone, email",
         [username, hashPassword, full_name, phone, email]
       );
       // Связывание пользователя с ролью
@@ -31,26 +42,65 @@ class AuthController {
         [user.rows[0].id, userRoleId.rows[0].id]
       );
 
-      return res
-        .status(200)
-        .json({ message: "Пользователь успешно зарегистрирован" });
+      return res.status(200).json({
+        response: {
+          status: 200,
+        },
+        message: "Пользователь успешно зарегистрирован",
+        payload: user.rows[0],
+      });
     } catch (e) {
       console.log(e);
-      res.status(400).json({ message: "Registration error" });
+      res.status(400).json({
+        response: {
+          status: 400,
+        },
+        message: "Ошибка регистрации",
+      });
     }
   }
 
-  async login() {
+  async login(req, res) {
     try {
-      res.json("login request");
+      const { username, password } = req.body;
+      const user = await db.query(
+    `
+      SELECT users.id, users.password, ARRAY_AGG(roles.value) AS roles
+      FROM users
+      LEFT JOIN user_roles ON users.id = user_roles.user_id
+      LEFT JOIN roles ON user_roles.role_id = roles.id
+      WHERE users.username = $1
+      GROUP BY users.id
+    `,
+        [username]
+      );
+      if (user.rows.length === 0) {
+        return res.status(400).json({
+          response: { status: 400 },
+          message: `Пользователь ${username} не найден`,
+        });
+      }
+      const validPassword = bcrypt.compareSync(password, user.rows[0].password);
+      if (!validPassword) {
+        return res.status(400).json({
+          response: { status: 400 },
+          message: "Введен неверный пароль",
+        });
+      }
+      const token = generateAccessToken(user.rows[0].id, user.rows[0].roles);
+      return res.json({token});
     } catch (e) {
       console.log(e);
-      res.status(400).json({ message: "Login error" });
+      res
+        .status(400)
+        .json({ response: { status: 400 }, message: "Ошибка авторизации" });
     }
   }
 
   async getUsers(req, res) {
     try {
+        const users = await db.query('SELECT * FROM users');
+        res.status(200).json({response: { status: 200 }, payload: users.rows});
     } catch (e) {
       console.log(e);
     }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 const Router = require("express");
 const router = new Router();
 const authController = require("../controllers/auth.controller.js");
+// const authMiddleware = require("../middleware/authMiddleware.js");
+const roleMiddleware = require("../middleware/roleMiddleware.js");
 
 router.post("/registration", authController.registration);
 router.post("/login", authController.login);
-router.get("/users", authController.getUsers);
+router.get("/users", roleMiddleware(['ADMIN']), authController.getUsers);
 
 module.exports = router;
